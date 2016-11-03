b429c51c199e0abf01f175b2bb19f93230909a32
pgilsena <pgilsena@tcd.ie>
2016-11-03 19:39:08 +0000

    change of ip address

--- lab1.py
+++ lab1.py
@@ -2,11 +2,11 @@ import socket
  
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
  
-host = 'localhost'
+host = '10.62.0.166'
 port = 8000
 s.connect((host , port))
 
-text = "hello"
+text = "make this caps"
 message = "GET /echo.php/?message=%s HTTP/1.1\r\n\r\n" % text 
 s.sendall(message)
 response = s.recv(1046)
