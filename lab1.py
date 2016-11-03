f46217d04335d3e2172a1d4dd7dbbbd363331e0d
pgilsena <pgilsena@tcd.ie>
2016-11-03 13:09:27 +0000

    Lab 1
    Simple echo server

---
+++ lab1.py
@@ -0,0 +1,15 @@
+import socket
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
+host = 'localhost'
+port = 8000
+s.connect((host , port))
+
+text = "hello"
+message = "GET /echo.php/?message=%s HTTP/1.1\r\n\r\n" % text 
+s.sendall(message)
+response = s.recv(1046)
+
+print response
+s.close()
