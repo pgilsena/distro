75ae048e0ab91e254eb52ff0962c6a11195b10c9
pgilsena <pgilsena@tcd.ie>
2016-11-11 00:34:17 +0000

    threads and responds to tests

--- lab2/server.py
+++ lab2/server.py
@@ -1,37 +1,32 @@
 import socket
 import threading
 
+S_NUM = 13325655
 
-class ClientThread(threading.Thread):
-
-    def __init__(self, ip, port):
-        threading.Thread.__init__(self)
-        self.ip = ip
+class Server(object):
+    def __init__(self, host, port):
+        self.host = '10.62.0.166'
         self.port = port
-        print "Message source: %s:%s" % (ip, str(port))
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((self.host, self.port))
 
-    def run(self):
+    def listen(self):
+        self.sock.listen(5)
         while True:
-            data = conn.recv(2048)
-            print "Server received data:", data
-            MESSAGE = raw_input("Message response: ")
-            if MESSAGE == 'exit':
-                break
-            conn.send(MESSAGE)
+            client, addr = self.sock.accept()
+            threading.Thread(self.listenToClient(client,addr)).start()
+        self.sock.close()
 
-host = 'localhost'
-port = 8000
+    def listenToClient(self, client, addr):
+        size = 1024
+        while True:
+            msg = client.recv(size)
+            if msg != 'KILL_SERVICE':
+                response = 'HELO BASE_TEST\nIP:%s\nPort:%d\nStudentID:%s' % (self.host, self.port, S_NUM)
+                client.send(response)
+            else:
+                client.close()
+                return False
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((host, port))
-threads = []
-
-while True:
-    s.listen(5)
-    (conn, (ip, port)) = s.accept()
-    newthread = ClientThread(ip, port)
-    newthread.start()
-    threads.append(newthread)
-
-for t in threads:
-    t.join()
+if __name__ == "__main__":
+    Server('localhost',8001).listen()
