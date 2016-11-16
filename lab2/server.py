1c9ae012486e5ed8b014216207b3f12d2cce9269
pgilsena <pgilsena@tcd.ie>
2016-11-16 12:38:00 +0000

    updated server.py

--- lab2/server.py
+++ lab2/server.py
@@ -1,32 +1,45 @@
 import socket
+import pdb
 import threading
+import sys
 
 S_NUM = 13325655
 
-class Server(object):
-    def __init__(self, host, port):
-        self.host = '10.62.0.166'
+class MyThread(threading.Thread):
+
+    def __init__(self,ip,port):
+        threading.Thread.__init__(self)
+        self.ip = ip
         self.port = port
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind((self.host, self.port))
 
-    def listen(self):
-        self.sock.listen(5)
-        while True:
-            client, addr = self.sock.accept()
-            threading.Thread(self.listenToClient(client,addr)).start()
-        self.sock.close()
-
-    def listenToClient(self, client, addr):
-        size = 1024
-        while True:
-            msg = client.recv(size)
-            if msg != 'KILL_SERVICE':
-                response = 'HELO BASE_TEST\nIP:%s\nPort:%d\nStudentID:%s' % (self.host, self.port, S_NUM)
-                client.send(response)
+    def run(self):
+       while True:
+            msg = conn.recv(2048)
+            if msg != 'KILL_SERVICE\n':
+                response = 'HELO BASE_TEST\nIP:%s\nPort:%d\nStudentID:%s' % (my_ip, my_port, S_NUM)
+                conn.send(response)
             else:
-                client.close()
-                return False
+                print "should be breaking here"
+                tcpServer.close()
+                break
 
-if __name__ == "__main__":
-    Server('localhost',8001).listen()
+my_ip = '10.62.0.166'
+my_port =  int(sys.argv[1])
+
+tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+tcpServer.bind((my_ip, my_port))
+threads = []
+
+while True:
+    tcpServer.listen(4)
+    (conn, (ip,port)) = tcpServer.accept()
+    newthread = MyThread(ip,port)
+    newthread.start()
+    threads.append(newthread)
+
+tcpServer.close()
+conn.close()
+
+for t in threads:
+    t.join()
