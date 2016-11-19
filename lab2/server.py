6e062981623dc01036cf7f4e6a1e168caa0d58d8
pgilsena <pgilsena@tcd.ie>
2016-11-19 22:18:36 +0000

    change of approach

--- lab2/server.py
+++ lab2/server.py
@@ -1,45 +1,57 @@
-import socket
+import logging
 import pdb
-import threading
+import socket
 import sys
+import threading
 
-S_NUM = 13325655
-
-class MyThread(threading.Thread):
-
-    def __init__(self,ip,port):
-        threading.Thread.__init__(self)
-        self.ip = ip
-        self.port = port
-
-    def run(self):
-       while True:
-            msg = conn.recv(2048)
-            if msg != 'KILL_SERVICE\n':
-                response = 'HELO BASE_TEST\nIP:%s\nPort:%d\nStudentID:%s' % (my_ip, my_port, S_NUM)
-                conn.send(response)
-            else:
-                print "should be breaking here"
-                tcpServer.close()
-                break
-
-my_ip = '10.62.0.166'
-my_port =  int(sys.argv[1])
-
-tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-tcpServer.bind((my_ip, my_port))
+s_num = 13325655
 threads = []
 
-while True:
-    tcpServer.listen(4)
-    (conn, (ip,port)) = tcpServer.accept()
-    newthread = MyThread(ip,port)
-    newthread.start()
-    threads.append(newthread)
+class ThreadedServer(object):
+    global threads
 
-tcpServer.close()
-conn.close()
+    def __init__(self, ip, port):
+        logging.info("Creating new thread...")
+        self.ip = ip
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.ip, self.port))
+        # logging.info("Server socket created")
 
-for t in threads:
-    t.join()
+    def serverListen(self):
+        self.sock.listen(5)
+        logging.info("Listening...")
+
+        while True:
+            conn, address = self.sock.accept()
+            newthread = threading.Thread(self.clientListen(conn, address))
+            newthread.start()
+            threads.append(newthread)
+            #logging.info(" newthread started: %d") % len(threads)
+
+    def clientListen(self, conn, address):
+        size = 1024
+        while True:
+            msg = conn.recv(size)
+            if msg != 'KILL_SERVICE\n':
+                # logging.info("Message: %s" % msg)
+                response = 'HELO BASE_TEST\nIP:%s\nPort:%d\nStudentID:%s' % (self.ip, self.port, S_NUM)
+                conn.send(response)
+            else:
+                logging.info("Message: %s" % msg)
+                self.close_everything()
+                return
+
+    def close_everything(self):
+        # pdb.set_trace()
+        for t in threads:
+            t.join()
+        self.sock.close()
+        logging.info("Everything closed")
+        sys.exit()
+
+if __name__ == "__main__":
+    logging.basicConfig(filename='lab2.log',level=logging.DEBUG)
+    port = int(sys.argv[1])
+    ThreadedServer('10.62.0.166', port).serverListen()
