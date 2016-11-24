68e24559c64e7eb011e6a26585870bf8d628e3f0
pgilsena <pgilsena@tcd.ie>
2016-11-24 18:42:28 +0000

    absolute mess, working on cleaning it up

--- lab3/server.py
+++ lab3/server.py
@@ -8,9 +8,73 @@ import threading
 
 s_num = 13325655
 ALIVE = True
-ROOMS = []
-USERS = []
+ROOMS = [] # list of Room objects?
+USERS = [] #
 ROOMS_AND_USERS = []
+
+
+class User():
+    def __init__(self, name, conn):
+        self.name = name
+        self.conn = conn
+
+    def getUserPair(self, name=None, ref=None):
+        return
+
+    def createUser(self, client_name):
+        global USERS
+
+        logging.error("USER: Creating new user")
+        idx = len(USERS)
+        USERS.append([idx, client_name, self.conn]) # fix this
+        return [idx, client_name]
+
+    def deleteUser():
+        # TODO
+        return
+
+
+class Room():
+    def __init__(self, ref, name):
+        self.ref = ref
+        self.name = name
+
+    def createRoom():
+        logging.error("ROOM: Creating new room")
+        idx = len(ROOMS)
+        ROOMS.append([idx, room_name]) # fix this
+        ROOMS_AND_USERS.append([idx, []])
+        return [idx, room_name]
+
+    def deleteRoom():
+        # TODO: remove room
+        return
+
+    def populateRoom():
+        if user_pair[0] in ROOMS_AND_USERS[room_pair[0]][1]:
+            logging.error("User already in room")
+            return
+        else:
+            logging.error("Adding user to room")
+            ROOMS_AND_USERS[room_pair[0]][1].append(user_pair[0])
+        return
+
+    def existsRoom():
+        tmp = len(ROOMS)
+        if tmp > 0:
+            for i in range(len(ROOMS)):
+                if ROOMS[[i][1]] == room:
+                    return True
+        else:
+            return False
+        return
+
+    def getRoomMembers():
+        # TODO: return a list of users in room
+        return
+
+    def sendMessageToRoom():
+        return.
 
 
 class ClientThread(threading.Thread):
@@ -22,9 +86,13 @@ class ClientThread(threading.Thread):
         self.address = address
         self.ip = address[0]
         self.port = address[1]
+        self.userName = ''
+        self.userRef = 0
         logging.error("%s - THREAD: Client thread created" % datetime.datetime.now())
 
     def run(self):
+        usr = User.createUser('tempName') # create user but will need to allow the change of name
+        rooms = []
         global ALIVE
         while True:
             data = self.conn.recv(2048)
@@ -32,19 +100,31 @@ class ClientThread(threading.Thread):
                 logging.error('CLIENT: HELO message')
                 response = 'HELO BASE_TEST\nIP:%s\nPort:%d\nStudentID:%d' % ('10.62.0.166', 8008, 13325655)
                 self.conn.send(response)
-            elif data == 'KILL_SERVICE\n':
-                logging.error('CLIENT: kill message')
+            elif 'JOIN_CHATROOM' in data:
+                logging.error("CLIENT: going to join chatroom")
+                msg = self.breakdownMessage(data)
+                usr.createUser(msg[3])
+                # update User name to be msg[3]
+
+                # check if msg[0] (room) exists
+                #   else create the room
+                # then add user to room
+                # send user the response below
+                response = "JOINED_CHATROOM:%s\nSERVER_IP:%s\nPORT:%d\nROOM_REF:%d\nJOIN_ID:%s\n" % room, self.ip, self.ip, room_pair[0], user_pair[1]
+                self.conn.send(response)
+            elif 'CHAT' in data:
+                # TODO: ogging.error('CLIENT: kill message')
                 ALIVE = False
                 self.conn.close()
                 return
-            elif 'JOIN_CHATROOM' in data:
-                logging.error("CLIENT: going to join chatroom")
-                self.connectToRoom(data)
+            elif 'DISCONNECT' in data:
+                # TODO
+                return
             else:
                 return
         return
 
-    def connectToRoom(self, data):
+    def breakdownMessage(self, data):
         logging.error("Connecting client to room")
         try:
             data = data.splitlines()
@@ -52,77 +132,10 @@ class ClientThread(threading.Thread):
             ip = data[1].replace('CLIENT_IP: ', '')
             port = data[2].replace('PORT: ', '')
             client_name = data[3].replace('CLIENT_NAME: ', '')
-
-            if self.check_room_exists(room) == False:
-                room_pair = self.create_new_room(room)
-            user_pair = self.add_new_user(client_name)
-            self.add_user_to_room(room_pair, user_pair)
-            logging.error("Added user to room")
-            pdb.set_trace()
-
-            response = "JOINED_CHATROOM:%s\nSERVER_IP:%s\nPORT:%d\nROOM_REF:%d\nJOIN_ID:%s\n" % room, self.ip, self.ip, room_pair[0], user_pair[1]
-            self.conn.send(response)
+            return [room, ip, port, client_name]
         except:
             logging.error("Something wrong with message")
             return
-
-    def check_room_exists(self, room):
-        tmp = len(ROOMS)
-        if tmp > 0:
-            for i in range(len(ROOMS)):
-                if ROOMS[[i][1]] == room:
-                    return True
-        else:
-            return False
-
-    def create_new_room(self, room_name):
-        logging.error("ROOM: Creating new room")
-        idx = len(ROOMS)
-        ROOMS.append([idx, room_name]) # fix this
-        ROOMS_AND_USERS.append([idx, []])
-        return [idx, room_name]
-
-    def add_new_user(self, client_name):
-        logging.error("USER: Creating new user")
-        idx = len(USERS)
-        USERS.append([idx, client_name, self.conn]) # fix this
-        return [idx, client_name]
-
-    def add_user_to_room(self, room_pair, user_pair):
-        if user_pair[0] in ROOMS_AND_USERS[room_pair[0]][1]:
-            logging.error("User already in room")
-            return
-        else:
-            logging.error("Adding user to room")
-            ROOMS_AND_USERS[room_pair[0]][1].append(user_pair[0])
-            #self.conn.send("%s has joined this chatroom\n" % user_pair[1])
-
-
-    def rename_this():
-        while 1:
-        socket_list = [sys.stdin, s]
-
-        # Get the list sockets which are readable
-        ready_to_read,ready_to_write,in_error = select.select(socket_list , [], [])
-
-        for sock in ready_to_read:
-            if sock == s:
-                # incoming message from remote server, s
-                data = sock.recv(4096)
-                if not data :
-                    print '\nDisconnected from chat server'
-                    sys.exit()
-                else :
-                    #print data
-                    sys.stdout.write(data)
-                    sys.stdout.write('[Me] '); sys.stdout.flush()
-
-            else :
-                # user entered a message
-                msg = sys.stdin.readline()
-                s.send(msg)
-                sys.stdout.write('[Me] '); sys.stdout.flush()
-
 
 #logging.basicConfig(filename='log.log', filemode='w', level=logging.DEBUG)
 my_port = int(sys.argv[1])
